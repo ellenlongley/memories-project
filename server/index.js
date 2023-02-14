4ac97fd8a9048043b7845f5d1c3bedbbee087339
Ellen Longley <ellenharperlongley@gmail.com>
2023-02-14 11:23:38 -0500

    message

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+
 import cors from "cors";
 
 import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://ellenlongley:ellenlongley123123@cluster0.ntawd0w.mongodb.net/?retryWrites=true&w=majority";
